b07ac2a3a7661f8adcc2c340a4dfab2e2559c2fd
faraaz-ahmed <faraazahmed000@gmail.com>
2019-08-27 11:26:30 +0530

    abrar

---
+++ front-end/src/app/classes/Validations.ts
@@ -0,0 +1,25 @@
+export class Validations{
+    validateString(s: String): boolean{
+        if(s.length >= 6){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    validatePhoneNumber(s: String): boolean{
+        for (var i = 0; i<s.length; i++){
+            if(s.charAt(i) < '0' && s.charAt(i) > '9'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    validateEmail(mail: String) {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)){
+            return true;
+        }
+        return false;
+    }
+}
